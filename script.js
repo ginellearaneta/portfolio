4ee87cf647e5f89aac034ffc910882f8cf7adb78
Ginelle Araneta <ginellegracearaneta@gmail.com>
2025-04-30 20:54:54 +0800

    Updated as of 04/30/2025

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Developer"],
+        strings: ["Software Developer", "Web Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
